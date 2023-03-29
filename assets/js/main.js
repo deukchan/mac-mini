1fd879166b2317c627e508546da35095bbb81a65
DEUKCHAHN <deukchan96@naver.com>
2023-03-29 16:35:30 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+$(function(){
+
+
+
+
+  /**
+   * @메뉴호버하면나오는서브메뉴
+   */
+  $('.nav-item.store').hover(function() {
+    $('.sublist').css('visibility', 'visible')
+  },function() {
+    $('.sublist').css('visibility', 'hidden')
+    
+  })
+
+  /**
+   * @모바일햄버거메뉴
+   */
+  $('.btn-close').click(function(e){
+    e.preventDefault();
+    $('.menu-list').css("visibility", "hidden");
+    $('.menu-list').css("opacity", '0');
+  })
+  $('.nav-item.menu').click(function(e){
+    e.preventDefault();
+    $('.menu-list').css("visibility", 'visible');
+    $('.menu-list').css("opacity", '1');
+  })
+
+
+  /**
+   * @lnb안에있는util버튼누르면나오는메뉴
+   */
+  $('.btn-util').click(function(e){
+    e.preventDefault();
+    if ($('.lnb-item .sub-item').hasClass('active')) {
+      $('.lnb-item .sub-item').removeClass('active');
+      $('.btn-util').css('transform', 'rotateX(360deg)');
+    }else{
+      $('.lnb-item .sub-item').addClass('active');
+      $('.btn-util').css('transform', 'rotateX(180deg)');
+    }
+  })
+
+  /**
+   * @versatility
+   */
+  $('.sc-versatility .tab.productivity').click(function(e){
+    e.preventDefault();
+    $('.img-wrap.productivity').css("display", "block");
+    $('.img-wrap.video').css("display", "none");
+  })
+
+  $('.sc-versatility .tab.video').click(function(e){
+    e.preventDefault();
+    $('.img-wrap.productivity').css("display", "none");
+    $('.img-wrap.video').css("display", "block");
+  })
+
+
+
+  /**
+   * @connection
+   */
+  $('.sc-connection .m2').click(function(e){
+    e.preventDefault();
+    $('.img-wrap.m2p').css("display", "block");
+    $('.img-wrap.m2').css("display", "none");
+  })
+
+  $('.sc-connection .m2p').click(function(e){
+    e.preventDefault();
+    $('.img-wrap.m2p').css("display", "none");
+    $('.img-wrap.m2').css("display", "block");
+  })
+
+
+
+  /**
+   * @footer
+   */
+  $('.footer .link-title').click(function() {
+    if ($(this).hasClass('shopping')) {
+      $('.shopping-list').toggle();
+    }
+    if ($(this).hasClass('wallet')) {
+      $('.wallet-list').toggle();
+    }
+    if ($(this).hasClass('account')) {
+      $('.account-list').toggle();
+    }
+    if ($(this).hasClass('entertain')) {
+      $('.entertain-list').toggle();
+    }
+    if ($(this).hasClass('store')) {
+      $('.store-list').toggle();
+    }
+    if ($(this).hasClass('business')) {
+      $('.business-list').toggle();
+    }
+    if ($(this).hasClass('education')) {
+      $('.education-list').toggle();
+    }
+    if ($(this).hasClass('values')) {
+      $('.values-list').toggle();
+    }
+    if ($(this).hasClass('info')) {
+      $('.info-list').toggle();
+    }
+  })
+
+})
